30b7835e85fb2abb7b54a56fd4be92789b7e9914
HaIIucination <karthik22kallakuri@gmail.com>
2024-05-05 13:25:16 +0530

    Multi file compilation

---
+++ Multi_file_comp/math.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include "myMath.h"
+
+
+int main()
+{
+  int n;
+  std::cin >> n;
+  std::cout << factorial(n) << std::endl;
+
+  return 0;
+}
---
+++ Multi_file_comp/myMath.h
@@ -0,0 +1,6 @@
+#ifndef MY_MATH
+#define MY_MATH
+
+long factorial(int n);
+
+#endif
